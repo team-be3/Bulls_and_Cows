f5074a1bd1eb9b9f29d95b79fe4b0b240fe7269c
ImaginaryP <seung589519@gmail.com>
2023-05-01 20:05:00 +0900

    feat: add Input Function
    add Input Function

--- do_Bulls.py
+++ do_Bulls.py
@@ -7,7 +7,19 @@ def generateAnswer():
         randomNum+=str(random.randint(0,9))
     return randomNum
 
+#인풋 받는 함수
+def inputNum():
+    print("숫자를 입력해주세요.")
+    while True:
+        num = input()
+        if((not num.isdigit()) or len(num) != 4):
+            print("잘못된 숫자입니다. 숫자를 다시 입력해주세요.")
+        else:
+            break
+    return num
+
 if __name__ == '__main__':
     answer = generateAnswer()
-    print(answer)
+    inNum = inputNum()
+    print(inNum)
 
