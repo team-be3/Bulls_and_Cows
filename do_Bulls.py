ef71b0979ca72dca4428eafbd49b5c1c9985d365
Jin_00 <anjdal64@gmail.com>
2023-05-01 21:11:41 +0900

    feat : 정답 기능 구현

--- do_Bulls.py
+++ do_Bulls.py
@@ -18,8 +18,27 @@ def inputNum():
             break
     return num
 
+#정답 여부 판별 함수
+def checkAnswer(inputNum, answer):
+    strike = 0
+    ball = 0
+    for i in range(len(answer)):
+        if inputNum[i] == answer[i]:
+            strike += 1
+        elif inputNum[i] in answer:
+            ball += 1
+    if strike == 4:
+        print("정답입니다!")
+    else:
+        print("스트라이크: {}, 볼: {}".format(strike, ball))
+
 if __name__ == '__main__':
     answer = generateAnswer()
     inNum = inputNum()
     print(inNum)
+    while True:
+        inNum = inputNum()
+        checkAnswer(inNum, answer)
+        if checkAnswer(inNum, answer) == "정답입니다!":
+            break
 
